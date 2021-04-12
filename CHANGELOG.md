d1ff41db06a1f47f25d76ce83e5e7ee4bfddddd0
DES-Destry <111andreyaa@gmail.com>
2021-04-12 15:41:12 +0600

    Prepare application for v1.0.1 release;

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Versions
 
+## v1.0.1
+- Now in host input user can type Domain name(in the previous version user can type only IP address)
+- Stop button added. If you realized that you don't need to scan everything, but realized it too late, then you can complete the process. But the check of those ports that have started to be checked will not stop and you will have to wait for some time.
+- Scanning has been significantyly accelerated. For scanning each port uses all available processors cores. In my case with Intel i5-2300 (4 cores) the speed increased by 4 times. This means that speed of scanning depends on cores count of your processor.
+
 ## v1.0.0
 - First release version.
 - Very simple port scanning with inputs for separated ports or range of ports.
--- Looto/Properties/AssemblyInfo.cs
+++ Looto/Properties/AssemblyInfo.cs
@@ -2,9 +2,7 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// Общие сведения об этой сборке предоставляются следующим набором
-// набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные со сборкой.
+
 [assembly: AssemblyTitle("Looto - port scanner")]
 [assembly: AssemblyDescription("Looto - powerful and user-friendly GUI for fast port scanning. This software can use ONLY for educational purposes or for protection purposes!!!")]
 [assembly: AssemblyConfiguration("Release")]
@@ -14,40 +12,14 @@ using System.Windows;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
-// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// из модели COM, установите атрибут ComVisible для этого типа в значение true.
 [assembly: ComVisible(false)]
 
-//Чтобы начать создание локализуемых приложений, задайте
-//<UICulture>CultureYouAreCodingWith</UICulture> в файле .csproj
-//в <PropertyGroup>. Например, при использовании английского (США)
-//в своих исходных файлах установите <UICulture> в en-US.  Затем отмените преобразование в комментарий
-//атрибута NeutralResourceLanguage ниже.  Обновите "en-US" в
-//строка внизу для обеспечения соответствия настройки UICulture в файле проекта.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
 [assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //где расположены словари ресурсов по конкретным тематикам
-                                     //(используется, если ресурс не найден на странице,
-                                     // или в словарях ресурсов приложения)
-    ResourceDictionaryLocation.SourceAssembly //где расположен словарь универсальных ресурсов
-                                              //(используется, если ресурс не найден на странице,
-                                              // в приложении или в каких-либо словарях ресурсов для конкретной темы)
+    ResourceDictionaryLocation.None, 
+    ResourceDictionaryLocation.SourceAssembly
 )]
 
-
-// Сведения о версии для сборки включают четыре следующих значения:
-//
-//      Основной номер версии
-//      Дополнительный номер версии
-//      Номер сборки
-//      Номер редакции
-//
-// Можно задать все значения или принять номера сборки и редакции по умолчанию 
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
