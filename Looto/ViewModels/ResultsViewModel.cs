a60fe2ef933b36cfeabcd720ca257da274869b5a
DES-Destry <111andreyaa@gmail.com>
2021-04-18 14:35:09 +0600

    Revert "Edit: results window was restyled; Edit: LAN list was small restyled; Edit: now error occured window open as dialog;"
    This reverts commit c466cec497f6e764d1a86893db57a6ac048a050f.

--- Looto/ViewModels/ResultsViewModel.cs
+++ Looto/ViewModels/ResultsViewModel.cs
@@ -1,8 +1,6 @@
 ﻿using Looto.Models.PortScanner;
-using System;
 using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace Looto.ViewModels
 {
@@ -13,9 +11,9 @@ namespace Looto.ViewModels
     class ResultsViewModel : BaseViewModel
     {
         #region Fields for binding
+        private readonly string _hostState;
         private ScanResult _result;
         private Brush _hostColor;
-        private BitmapImage _scanImage;
 
         private bool _isLoading = true;
         private int _currentProgress = 1;
@@ -36,7 +34,7 @@ namespace Looto.ViewModels
         }
         /// <summary>User-friendly host information.</summary>
         /// <value>The <see cref="Host"/> property gets the value of the <see cref="ScanResult.Host"/> field, <see cref="_result"/>.</value>
-        public string Host => $"{_result.Host}";
+        public string Host => $"Scanned host: {_result.Host} {_hostState}";
         /// <summary>Color of host string.</summary>
         /// <value>The <see cref="HostColor"/> property gets/sets the value of the <see cref="Brush"/> field, <see cref="_hostColor"/>.</value>
         public Brush HostColor
@@ -48,22 +46,9 @@ namespace Looto.ViewModels
                 OnPropertyChanged();
             }
         }
-        /// <summary>Image in the border.</summary>
-        /// <value>The <see cref="ScanImage"/> property gets/sets the value of the <see cref="BitmapImage"/> field, <see cref="_scanImage"/>.</value>
-        public BitmapImage ScanImage
-        {
-            get => _scanImage;
-            set
-            {
-                _scanImage = value;
-                OnPropertyChanged();
-            }
-        }
         /// <summary>User-friendly scanned date information.</summary>
         /// <value>The <see cref="Host"/> property gets the value of the <see cref="ScanResult.ScanDate"/> field, <see cref="_result"/>.</value>
         public string ScanDate => $"Scanned at: {_result.ScanDate:G}";
-        public bool IsValidHost => _result.HostIsValid;
-        public bool IsNotValidHost => !_result.HostIsValid;
 
         /// <summary>If result not rendered yet.</summary>
         /// <value>The <see cref="IsLoading"/> property gets/sets the value of the <see cref="bool"/> field, <see cref="_isLoading"/>.</value>
@@ -110,9 +95,8 @@ namespace Looto.ViewModels
             HostColor = _result.HostIsValid ?
                 (Brush)Application.Current.MainWindow.FindResource("WhiteBrush")
                 : (Brush)Application.Current.MainWindow.FindResource("RedBrush");
-            ScanImage = _result.HostIsValid ?
-                new BitmapImage(new Uri("/Looto;component/Images/scan_green.png", UriKind.Relative))
-                : new BitmapImage(new Uri("/Looto;component/Images/scan_red.png", UriKind.Relative));
+
+            _hostState = _result.HostIsValid ? string.Empty : "(DOESN'T EXISTS)";
         }
     }
 }
