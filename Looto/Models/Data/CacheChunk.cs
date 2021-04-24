934d3e7371ca045135c3097803798c68087bcb66
DES-Destry <111andreyaa@gmail.com>
2021-04-24 21:36:55 +0600

    New: classes for using app scan cache was added;

---
+++ Looto/Models/Data/CacheChunk.cs
@@ -0,0 +1,23 @@
+﻿using Looto.Models.PortScanner;
+using System;
+using System.Collections.Generic;
+
+namespace Looto.Models.Data
+{
+    /// <summary>Object of cache data.</summary>
+    [Serializable]
+    class CacheData
+    {
+        /// <summary>Life time of the one chunck.</summary>
+        public TimeSpan ChunckLifetime { get; set; }
+        /// <summary>Chuncks of results(cache).</summary>
+        public List<ScanResult> Chuncks { get; set; }
+
+        /// <summary>Create instance of cache data with defaults.</summary>
+        public CacheData()
+        {
+            Chuncks = new List<ScanResult>();
+            ChunckLifetime = TimeSpan.FromDays(3);
+        }
+    }
+}
