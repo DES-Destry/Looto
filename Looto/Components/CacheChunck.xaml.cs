51da9657b9711389047ce0a0014cdd9e8555832a
DES-Destry <111andreyaa@gmail.com>
2021-04-25 15:20:00 +0600

    New: scan history window has been added(but it not working correct);

---
+++ Looto/Components/CacheChunck.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Looto.Models.PortScanner;
+using Looto.Views;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Looto.Components
+{
+    /// <summary>User control to show one chunck of cache.</summary>
+    public partial class CacheChunck : UserControl
+    {
+        /// <summary>If it is darker - background will be darker, not fully transparent.</summary>
+        public bool IsDarker
+        {
+            get => (bool)GetValue(IsDarkerProperty);
+            set { SetValue(IsDarkerProperty, value); }
+        }
+
+        /// <summary>DP for <see cref="IsDarker"/> property.</summary>
+        public static readonly DependencyProperty IsDarkerProperty =
+            DependencyProperty.Register("IsDarker", typeof(bool), typeof(CacheChunck), new PropertyMetadata(false));
+
+
+
+        /// <summary>Scan result data from cache.</summary>
+        public ScanResult ScanResult
+        {
+            get => (ScanResult)GetValue(ScanResultProperty);
+            set { SetValue(ScanResultProperty, value); }
+        }
+
+        /// <summary>DP for <see cref="ScanResult"/> property.</summary>
+        public static readonly DependencyProperty ScanResultProperty =
+            DependencyProperty.Register("ScanResult", typeof(ScanResult), typeof(CacheChunck));
+
+
+
+        /// <summary>Calls when user clicked on delete button.</summary>
+        public event Action<ScanResult> OnDeleteClicked;
+
+        /// <summary>Create new component.</summary>
+        public CacheChunck()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>When grid, where placed all content was loaded, apply all DPs to show all info.</summary>
+        /// <param name="sender">Xaml element, which called this method.</param>
+        /// <param name="e">Some event arguments.</param>
+        private void MainGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Is is darker - made background slightly(127/255) transparent black color.
+            if (IsDarker)
+                MainGrid.Background = new SolidColorBrush(Color.FromArgb(0x7F, 0, 0, 0));
+
+            Host.Text = ScanResult.Host;
+            Time.Text = GetTimeString(ScanResult.ScanDate);
+        }
+
+        /// <summary>When user clicks at show button.</summary>
+        /// <param name="sender">Xaml element, which called this method(show button).</param>
+        /// <param name="e">Some event arguments.</param>
+        private void Show_Click(object sender, RoutedEventArgs e)
+        {
+            ResultsWindow view = new ResultsWindow(ScanResult);
+            view.Show();
+        }
+
+        /// <summary>When user clicks at delete button.</summary>
+        /// <param name="sender">Xaml element, which called this method(delete button).</param>
+        /// <param name="e">Some event arguments.</param>
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            OnDeleteClicked?.Invoke(ScanResult);
+        }
+
+        /// <summary>Generate string of time passed.</summary>
+        /// <param name="date">Date from.</param>
+        /// <returns>String of time passed.</returns>
+        private string GetTimeString(DateTime date)
+        {
+            TimeSpan time = date - DateTime.Now;
+
+            if (time.TotalDays > 0)
+                return $"{time.TotalDays} days ago.";
+            if (time.TotalHours > 0)
+                return $"{time.TotalHours} hours ago.";
+            if (time.TotalMinutes > 0)
+                return $"{time.TotalMinutes} minutes ago.";
+            if (time.TotalSeconds > 0)
+                return $"{time.TotalSeconds} seconds ago.";
+
+            return "Right now.";
+        }
+    }
+}
---
+++ Looto/ViewModels/ScanHistoryViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Looto.Models.Data;
+using System;
+
+namespace Looto.ViewModels
+{
+    public class ScanHistoryViewModel : BaseViewModel
+    {
+        private readonly Cache _cache;
+
+        /// <summary>Calls on data render request.</summary>
+        public event Action<CacheData> RenderCache;
+
+        /// <summary>Create new view model with cache.</summary>
+        /// <param name="cache">Cache from file.</param>
+        public ScanHistoryViewModel(Cache cache)
+        {
+            _cache = cache;
+            Render();
+        }
+
+        public void Render()
+        {
+            RenderCache?.Invoke(_cache.GetCache());
+        }
+    }
+}
